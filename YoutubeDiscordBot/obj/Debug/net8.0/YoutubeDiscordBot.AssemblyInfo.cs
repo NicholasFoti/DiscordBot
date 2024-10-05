577fbe133c2c95c95d7137300fd889d9ac816165
NicholasFoti <nickfoti@hotmail.com>
2024-10-05 22:25:43 +0800

    Update

--- YoutubeDiscordBot/obj/Debug/net8.0/YoutubeDiscordBot.AssemblyInfo.cs
+++ YoutubeDiscordBot/obj/Debug/net8.0/YoutubeDiscordBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YoutubeDiscordBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f990dab12a29eade5c740962999820a023a935b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e477960a498a186fb2884352ee90fa61765d875")]
 [assembly: System.Reflection.AssemblyProductAttribute("YoutubeDiscordBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YoutubeDiscordBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
